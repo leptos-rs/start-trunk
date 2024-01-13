5871950438ec11c902a405eadf520bda8d76e8ca
Diversable <dan.mantei@outlook.com>
2024-01-12 16:22:15 -0800

    add rust-toolchain file

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ fn App(increment: i32) -> impl IntoView {
     view! {
     <div class="container">
 
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
+        <picture>
+            <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+            <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+        </picture>
 
         <h1>"Welcome to Leptos"</h1>
 
