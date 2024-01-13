73298d507bbf44f1bdb8789cbe8f95e6358fedc7
Diversable <dan.mantei@outlook.com>
2024-01-12 17:24:01 -0800

    Include 2 buttons w/ optional props

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use leptos::*;
 use leptos_router::*;
 
+/// A router which renders the homepage and handles 404's
 #[component]
 fn App() -> impl IntoView {
     view! {
         <Router>
             <Routes>
                 <Route path="/" view=Home /> />
-                <Route path="/*any" view=|| view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  } />
+                <Route path="/*" view=|| view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  } />
             </Routes>
         </Router>
     }
 }
 
+/// Default homepage
 #[component]
 fn Home() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-    let increment = 1;
-
     view! {
         <div class="container">
 
@@ -28,17 +27,27 @@ fn Home() -> impl IntoView {
 
             <h1>"Welcome to Leptos"</h1>
 
-            <button
-                on:click= move |_| {
-                    set_count(count() + increment)
-                }
-            >
-                "Click me: "
-                {count}
-            </button>
-
+            <div class="buttons">
+                <Button />
+                <Button increment=5 />
+            </div>
 
         </div>
+    }
+}
+
+/// A parameterized incrementing button
+#[component]
+fn Button(#[prop(default = 1)] increment: i32) -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+    view! {
+        <button
+            on:click= move |_| {
+                set_count(count() + increment)
+            }
+        >
+            "Click me: " {count}
+        </button>
     }
 }
 
