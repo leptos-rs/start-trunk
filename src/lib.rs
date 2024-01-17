fb8cdffe3df4a8e33076aa0a7beb0ccb90be682b
Diversable <dan.mantei@outlook.com>
2024-01-16 16:21:17 -0800

    rename homepage to home

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn App() -> impl IntoView {
 
         <Router>
             <Routes>
-                <Route path="/" view=App />
+                <Route path="/" view=Home />
                 <Route path="/*" view=NotFound />
             </Routes>
         </Router>
