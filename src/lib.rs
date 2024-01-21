078dbcc515c07f31e4215c09c09224b51b232753
Diversable <dan.mantei@outlook.com>
2024-01-20 16:22:49 -0800

    Update with injected metadata + error boundary

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,45 @@ use crate::pages::not_found::NotFound;
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+
     view! {
+
+        // injects info into HTML tag from application code
+        <Html
+            lang="en"
+            dir="ltr"
+            attr:data-theme="light"
+        />
 
         // sets the document title
         <Title text="Welcome to Leptos CSR"/>
 
-        // injects a stylesheet into the document <head>
-        <Stylesheet id="leptos" href="/{{crate_name}}.css"/>
+        // injects metadata in the <head> of the page
+        <Meta charset="UTF-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <Router>
-            <Routes>
-                <Route path="/" view=Home />
-                <Route path="/*" view=NotFound />
-            </Routes>
-        </Router>
+
+        <ErrorBoundary
+            fallback=|errors| view! {
+                <h1>"Uh oh! Something went wrong!"</h1>
+                <p>"Errors: "</p>
+                // Render a list of errors as strings - good for development purposes
+                <ul>
+                    {move || errors.get()
+                        .into_iter()
+                        .map(|(_, e)| view! { <li>{e.to_string()}</li>})
+                        .collect_view()
+                    }
+                </ul>
+            }
+        >
+            <Router>
+                <Routes>
+                    <Route path="/" view=Home />
+                    <Route path="/*" view=NotFound />
+                </Routes>
+            </Router>
+
+        </ErrorBoundary>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use leptos::*;
 use {{crate_name}}::App;
 
 fn main() {
+    // set up logging
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
     mount_to_body(|| {
         view! {
             <App />
