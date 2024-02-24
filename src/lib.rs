ab636d2432b5a583f90e153225ea79256803f587
victor <vjbsh@hotmail.com>
2024-02-24 17:19:38 -0600

    moved error boundary to avoid page reload

--- src/lib.rs
+++ src/lib.rs
@@ -17,44 +17,20 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-
-        // injects info into HTML tag from application code
-        <Html
-            lang="en"
-            dir="ltr"
-            attr:data-theme="light"
-        />
+        <Html lang="en" dir="ltr" attr:data-theme="light"/>
 
         // sets the document title
         <Title text="Welcome to Leptos CSR"/>
 
         // injects metadata in the <head> of the page
-        <Meta charset="UTF-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Meta charset="UTF-8"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-
-        <ErrorBoundary
-            fallback=|errors| view! {
-                <h1>"Uh oh! Something went wrong!"</h1>
-
-                <p>"Errors: "</p>
-                // Render a list of errors as strings - good for development purposes
-                <ul>
-                    {move || errors.get()
-                        .into_iter()
-                        .map(|(_, e)| view! { <li>{e.to_string()}</li>})
-                        .collect_view()
-                    }
-                </ul>
-            }
-        >
-            <Router>
-                <Routes>
-                    <Route path="/" view=Home />
-                    <Route path="/*" view=NotFound />
-                </Routes>
-            </Router>
-
-        </ErrorBoundary>
+        <Router>
+            <Routes>
+                <Route path="/" view=Home/>
+                <Route path="/*" view=NotFound/>
+            </Routes>
+        </Router>
     }
 }
--- src/pages/home.rs
+++ src/pages/home.rs
@@ -5,6 +5,22 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
+                <ErrorBoundary
+            fallback=|errors| view! {
+                <h1>"Uh oh! Something went wrong!"</h1>
+
+                <p>"Errors: "</p>
+                // Render a list of errors as strings - good for development purposes
+                <ul>
+                    {move || errors.get()
+                        .into_iter()
+                        .map(|(_, e)| view! { <li>{e.to_string()}</li>})
+                        .collect_view()
+                    }
+                </ul>
+            }
+        >
+
         <div class="container">
 
             <picture>
@@ -20,5 +36,6 @@ pub fn Home() -> impl IntoView {
             </div>
 
         </div>
+        </ErrorBoundary>
     }
 }
