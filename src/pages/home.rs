cc9743ef446522feb0305f83311393fc07ec801b
victor <vjbsh@hotmail.com>
2024-02-24 17:29:30 -0600

    formatting

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -5,37 +5,48 @@ use leptos::*;
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
-                <ErrorBoundary
-            fallback=|errors| view! {
+        <ErrorBoundary fallback=|errors| {
+            view! {
                 <h1>"Uh oh! Something went wrong!"</h1>
 
                 <p>"Errors: "</p>
                 // Render a list of errors as strings - good for development purposes
                 <ul>
-                    {move || errors.get()
-                        .into_iter()
-                        .map(|(_, e)| view! { <li>{e.to_string()}</li>})
-                        .collect_view()
-                    }
+                    {move || {
+                        errors
+                            .get()
+                            .into_iter()
+                            .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                            .collect_view()
+                    }}
+
                 </ul>
             }
-        >
+        }>
 
-        <div class="container">
+            <div class="container">
 
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
+                <picture>
+                    <source
+                        srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg"
+                        media="(prefers-color-scheme: dark)"
+                    />
+                    <img
+                        src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg"
+                        alt="Leptos Logo"
+                        height="200"
+                        width="400"
+                    />
+                </picture>
 
-            <h1>"Welcome to Leptos"</h1>
+                <h1>"Welcome to Leptos"</h1>
 
-            <div class="buttons">
-                <Button />
-                <Button increment=5 />
+                <div class="buttons">
+                    <Button/>
+                    <Button increment=5/>
+                </div>
+
             </div>
-
-        </div>
         </ErrorBoundary>
     }
 }
