1f160c6d3f744da41ffd813fee54a85573a13430
Anton <gh@gets.love>
2024-12-25 15:59:41 +0100

    use NotFound component directly in fallback

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ pub fn App() -> impl IntoView {
         <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <Router>
-            <Routes fallback=|| view! { <NotFound /> }>
+            <Routes fallback=NotFound>
                 <Route path=path!("/") view=Home />
             </Routes>
         </Router>
