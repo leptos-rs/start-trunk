80fdb59aa89b7d928646b75129a5be3cc2dbc0ff
Marius <mar.koeppen@gmail.com>
2025-08-30 23:23:42 +0200

    Changed 404 page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -3,5 +3,8 @@ use leptos::prelude::*;
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
-    view! { <h1>"Uh oh!" <br /> "We couldn't find that page!"</h1> }
+    view! {
+        <h1>"404: Page not found"</h1>
+        <h2>"We couldn't find that page!"</h2>
+    }
 }
